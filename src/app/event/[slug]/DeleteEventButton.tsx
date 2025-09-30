daaadc12c4a98bd27fc55c98a8574102e8c37624
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-09-30 10:24:13 -0700

    W.I.P - Delete Event button

--- src/app/event/[slug]/DeleteEventButton.tsx
+++ src/app/event/[slug]/DeleteEventButton.tsx
@@ -1,1 +1,11 @@
-'use client'
+'use client';
+import { useState } from 'react';
+import DeleteEventModal from './DeleteEventModal'
+
+interface DeleteEventButtonProps {
+    eventSlug: string
+}
+
+export default function DeleteEventButton({ eventSlug }: DeleteEventButtonProps) {
+    const [showModal, setShowModal] = useState(false)
+}
