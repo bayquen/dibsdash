7f4a0f5fc46aa1a75acf8cdcb93a2355a4684638
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-10-02 09:51:26 -0700

    W.I.P

---
+++ src/app/event/[slug]/DeleteItemButton.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+interface DeleteItemButtonProps {
+    itemId: string
+    itemName: string
+}
+
+export default function DeleteItemButton({ itemId, itemName }: DeleteItemButtonProps) {
+
+}
