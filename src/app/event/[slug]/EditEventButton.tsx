e79279f803bf1afc7047decd158d2e9821e3965a
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-10-03 12:01:08 -0700

    WIP Edit-Event button

--- src/app/event/[slug]/EditEventButton.tsx
+++ src/app/event/[slug]/EditEventButton.tsx
@@ -5,3 +5,11 @@ import EditEventModal from './EditEventModal';
 interface EditEventModalProps {
     event: any
 }
+
+export default function EditEventButton( { event }: EditEventModalProps) {
+    const [showModal, setShowModal] = useState(false)
+
+    return (
+        
+    )
+}
