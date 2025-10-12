cd21208650997bf4c76d82d32057267e1a6aaa09
Brandon Bayquen <93808808+bayquen@users.noreply.github.com>
2025-10-11 20:22:44 -0700

    Edit

--- src/app/event/[slug]/EventItemsTable.tsx
+++ src/app/event/[slug]/EventItemsTable.tsx
@@ -84,8 +84,8 @@ export default function EventItemsTable({ categorizedItems }: EventItemsTablePro
                         )}
                     </div>
                 )
-        })}
+                
+            })}
         </div>
     )
-
 }
