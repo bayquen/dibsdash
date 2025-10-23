91801d1d99b7e06ce911110ff08156ef05532481
Brandon <brandonbayquen@gmail.com>
2025-10-23 10:57:43 -0700

    Test - modify Next config file for TypeScript

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This tells Next.js your code is in src/
-  experimental: {
-    typedRoutes: false,
-  },
-};
-
-module.exports = nextConfig;
---
+++ next.config.ts
@@ -0,0 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   // This tells Next.js your code is in src/
+//   experimental: {
+//     typedRoutes: false,
+//   },
+// };
+
+// module.exports = nextConfig;
+
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  /* config options here */
+}
+ 
+export default nextConfig
