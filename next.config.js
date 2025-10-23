b6abb604c4b29a92da18d9d87e1ece2c137060ee
Brandon <brandonbayquen@gmail.com>
2025-10-23 10:23:51 -0700

    Add Next.js config file! Needed for deployment

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // This tells Next.js your code is in src/
+  experimental: {
+    typedRoutes: false,
+  },
+};
+
+module.exports = nextConfig;
