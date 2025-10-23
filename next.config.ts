1dd33769c14e1230b7d3491ef88504d371abd07b
Brandon <brandonbayquen@gmail.com>
2025-10-23 13:24:35 -0700

    Clean

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // This tells Next.js your code is in src/
-//   experimental: {
-//     typedRoutes: false,
-//   },
-// };
-
-// module.exports = nextConfig;
-
 import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
   /* config options here */
 }
  
-export default nextConfig 
+export default nextConfig
